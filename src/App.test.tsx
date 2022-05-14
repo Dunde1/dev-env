1072c18c4e083817606f0cf0858a52d3cb8f8b2f
origin <chldlsrb1000@naver.com>
2022-05-14 19:32:33 +0900

    test 작성 및 수행 - 시작시 테스트 성공 후 시작되는 스크립트 추가

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,55 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('TDD로 개발하는 counter 앱 만들기', () => {
+  test('the counter starts at 0', () => {
+    render(<App />);
+    const counterElement = screen.getByTestId('counter');
+    expect(counterElement).toHaveTextContent('0');
+  });
+
+  test('minus button has correct text', () => {
+    render(<App />);
+    const buttonElement = screen.getByTestId('minus-button');
+    expect(buttonElement).toHaveTextContent('-');
+  });
+
+  test('plus button has correct text', () => {
+    render(<App />);
+    const buttonElement = screen.getByTestId('plus-button');
+    expect(buttonElement).toHaveTextContent('+');
+  });
+
+  test('When the - button is pressed, the counter changes to -1', () => {
+    render(<App />);
+    const buttonElement = screen.getByTestId('minus-button');
+    fireEvent.click(buttonElement);
+    const counterElement = screen.getByTestId('counter');
+    expect(counterElement).toHaveTextContent('-1');
+  });
+
+  test('When the + button is pressed, the counter changes to 1', () => {
+    render(<App />);
+    const buttonElement = screen.getByTestId('plus-button');
+    fireEvent.click(buttonElement);
+    const counterElement = screen.getByTestId('counter');
+    expect(counterElement).toHaveTextContent('1');
+  });
+
+  test('on/off button has blue color', () => {
+    render(<App />);
+    const buttonElement = screen.getByTestId('on/off-button');
+    expect(buttonElement).toHaveStyle({ backgroundColor: 'blue' });
+  });
+
+  test('Prevent the -,+ button from being pressed when the on/off button is clicked', () => {
+    render(<App />);
+    const onOffButtonElement = screen.getByTestId('on/off-button');
+    fireEvent.click(onOffButtonElement);
+    const minusButtonElement = screen.getByTestId('minus-button');
+    const plusButtonElement = screen.getByTestId('plus-button');
+    expect(plusButtonElement).toBeDisabled();
+    expect(minusButtonElement).toBeDisabled();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [disabled, setDisabled] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h3 data-testid='counter'>{counter}</h3>
+        <div>
+          <button onClick={() => setCounter(counter - 1)} data-testid='minus-button' disabled={disabled}>-</button>
+          <button onClick={() => setCounter(counter + 1)} data-testid='plus-button' disabled={disabled}>+</button>
+        </div>
+        <div>
+          <button data-testid='on/off-button' style={{ backgroundColor: 'blue' }} onClick={() => setDisabled(!disabled)}>on/off</button>
+        </div>
       </header>
     </div>
   );
